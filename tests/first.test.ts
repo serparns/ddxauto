fdbfbd1486a745919593019e3380f67c0ee6e13d
Dmitrii Vorobyev <dvorobev@svyaznoy.ru>
2023-12-29 20:26:07 +0300

    авто комит

--- tests/first.test.ts
+++ tests/first.test.ts
@@ -3,7 +3,8 @@ import { test }  from "@playwright/test";
 test.describe("Описание группы теста", async () => {
     test("Я пишу тест",async () => {
         console.log("Уиии! Я написал тест!"),
-        console.log("Буду ждать продолжение уроков");
+        console.log("Буду ждать продолжение уроков"),
+        console.log("Спасибо, за урок, с наступающим!!!");
         
     })    
 })
