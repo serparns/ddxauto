145049c68ab2d59b6a8b0907885d48f9ab2b26b7
Dmitrii Vorobyev <dvorobev@svyaznoy.ru>
2023-12-29 20:21:46 +0300

    Буду использовать комиты через иде)), так удобней

--- tests/first.test.ts
+++ tests/first.test.ts
@@ -1,8 +1,9 @@
 import { test }  from "@playwright/test";
 
-test.describe("Описание группы теста",async () => {
+test.describe("Описание группы теста", async () => {
     test("Я пишу тест",async () => {
-        console.log("Уиии! Я написал тест!");
+        console.log("Уиии! Я написал тест!"),
+        console.log("Буду ждать продолжение уроков");
         
     })    
 })
