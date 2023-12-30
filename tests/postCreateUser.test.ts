7cf76fa6e3b6ed6a568b2aaedca286962b119e23
Dmitrii Vorobyev <dvorobev@svyaznoy.ru>
2023-12-30 20:10:04 +0300

    fix

--- tests/postCreateUser.test.ts
+++ tests/postCreateUser.test.ts
@@ -2,7 +2,7 @@ import { expect, test }  from "@playwright/test";
 import api from '../api.json';
 import { json, text } from "stream/consumers";
 import { getBaseParameters } from "../entities/baseParameters";
-import { getRandomPhoneNumber } from "../utils/random";
+import { getRandomEmail, getRandomPhoneNumber, randomSport_experience } from "../utils/random";
 
 test.describe("Api-тест на создание клиента", async () => {
     test("[positive] Создать нового клиента ",async ({ request }) => {
@@ -17,7 +17,7 @@ test.describe("Api-тест на создание клиента", async () => {
                 request_id: "321",
                 request_source: "crm",
                 data: {
-                    email: "ttt@test.test",
+                    email: getRandomEmail(),
                     name: "Имя",
                     last_name: "last_name",
                     middle_name: "string",
@@ -39,4 +39,78 @@ test.describe("Api-тест на создание клиента", async () => {
 
     expect(response.status()).toEqual(200);       
     });
+
+    test("[positive_no_pass] Создать нового клиента ",async ({ request }) => {
+        const response = await request.post(
+         `${api.urls.base_url_api}${api.paths.users}`,
+         {
+           headers: {
+            'Authorization': `${api.token.UltraToken}`
+            }, 
+           data: {
+                session_id: "123",
+                request_id: "321",
+                request_source: "crm",
+                data: {
+                    email: getRandomEmail(),
+                    name: "Имя",
+                    last_name: "last_name",
+                    middle_name: "string",
+                    sex: "male",
+                    phone: getRandomPhoneNumber(),
+                    birthday: "1999-11-11",
+                    lang: "ru",
+                    home_club_id: 1,
+                    club_access: true,
+                    admin_panel_access: true,
+                    group_training_registration_access: true,
+                    sport_experience: "Больше 5 лет"
+                }
+        
+            }
+         }    
+    );
+
+    expect(response.status()).toEqual(200);
+    console.log(await response.json())       
+    });
+
+    test("[positive_randomSport_experience] Создать нового клиента ",async ({ request }) => {
+        const response = await request.post(
+         `${api.urls.base_url_api}${api.paths.users}`,
+         {
+           headers: {
+            'Authorization': `${api.token.UltraToken}`
+            }, 
+           data: {
+                session_id: "123",
+                request_id: "321",
+                request_source: "crm",
+                data: {
+                    email: getRandomEmail(),
+                    name: "Имя",
+                    last_name: "last_name",
+                    middle_name: "string",
+                    sex: "male",
+                    phone: getRandomPhoneNumber(),
+                    birthday: "1999-11-11",
+                    password: "qwerty123",
+                    lang: "ru",
+                    home_club_id: 1,
+                    club_access: true,
+                    admin_panel_access: true,
+                    group_training_registration_access: true,
+                    sport_experience: randomSport_experience()
+                }
+        
+            }
+         }    
+    );
+
+    expect(response.status()).toEqual(200);
+    console.log(await response.json())       
+    });
+    
+
+    
 })
--- utils/random.ts
+++ utils/random.ts
@@ -3,5 +3,11 @@ export function getRandomPhoneNumber(): string{
 }
 
 export function getRandomEmail(): string{
-    return `qa${timestamp}@gmail.com`
+    return `qa${new Date().getTime().toString()}@gmail.com`
+}
+
+export function randomSport_experience(): string{
+const sport_experience = ['Нет опыта', '0-6 месяцев', 'Больше 5 лет', '2-3 года', '1-2 года', '3-5 лет'];
+const randomSport_experience = sport_experience[(Math.floor(Math.random() * (sport_experience.length)))];
+    return randomSport_experience
 }
