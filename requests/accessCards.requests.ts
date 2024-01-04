a1f9b119eb1faeddbd6d3e441aac4bc4bbae33d0
Vorobev Dmiyrii <serparns@gmail.com>
2024-01-04 23:59:04 +0300

    начало 5 урока

---
+++ requests/accessCards.requests.ts
@@ -0,0 +1,16 @@
+import BaseRequests from "./baseRequests.request";
+import {APIResponse} from "@playwright/test";
+import paths from '../api.json';
+import api from "../api.json";
+
+
+export default class  AccessCardsRequests extends BaseRequests {
+    async postAccessCards(status: number, body: object): Promise<APIResponse> {
+        return await this.post(`${this.baseUrl}${api.paths.access_cards}`, status, body);
+    }
+    async getAccessCardsById(status: number, parameters: object, accessCardId: number): Promise<APIResponse> {
+        return await this.get(`${this.baseUrl}${api.paths.access_cards}/${accessCardId}`, status, parameters);
+    }
+}
+
+
---
+++ requests/baseRequests.request.ts
@@ -0,0 +1,36 @@
+import {APIRequestContext, APIResponse, expect} from "@playwright/test";
+import paths from '../api.json';
+import {log} from "../utils/logger";
+
+export default class BaseRequests {
+    protected baseUrl: string;
+
+    constructor(
+        protected request: APIRequestContext,
+        private token = paths.token.test
+    ) {
+        this.baseUrl = paths.urls.base_url_api
+    }
+    async get(url: string, status: number, parameters?: any): Promise<APIResponse> {
+        log("request url", url);
+        log("parameters", parameters);
+        const response = await this.request.get(url, {headers: {'Authorization': this.token}, params: parameters});
+        log("response status", await response.status());
+        log("response body", JSON.stringify(await response.json(), null, '\t'));
+
+        expect(response.status()).toEqual(status);
+        return response;
+    }
+
+    async post(url: string, status: number, requestBody?: any): Promise<APIResponse> {
+        log("request url", url);
+        log("request body", JSON.stringify(await requestBody, null, '\t'));
+        const response = await this.request.post(url, {headers: {'Authorization': this.token}, data: requestBody});
+        log("response status", await response.status());
+        log("response body", JSON.stringify(await response.json(), null, '\t'));
+
+        expect(response.status()).toEqual(status);
+        return response;
+    }
+
+}
