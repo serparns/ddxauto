b85d6ef34c5c7a0968215f01f1afa526e569a990
Vorobev Dmiyrii <serparns@gmail.com>
2024-01-04 23:58:56 +0300

    начало 5 урока

--- entities/dataAccessCard.ts
+++ entities/dataAccessCard.ts
@@ -1,6 +1,4 @@
 import {getRandomCardNumber} from "../utils/random";
-import { type } from "../tests/postAccessCards.test";
-
 
 export async function dataParans(): Promise<object> {
     return {
@@ -8,14 +6,14 @@ export async function dataParans(): Promise<object> {
         request_id: "123",
         request_source: "123"
     }
+}
+export async function dataAccessCard(): Promise<object> {
+    return {
+        access_card_number: getRandomCardNumber(),
+        user_id: 1346578,
+        is_blocked: false,
+        is_lost: false,
+        is_deleted: false,
+        block_previous_card: false
     }
-    export async function dataAccessCard(): Promise<object> {
-        return {
-                access_card_number: getRandomCardNumber(),
-                user_id: 1346578,
-                is_blocked: false,
-                is_lost: false,
-                is_deleted: false,
-                block_previous_card: false
-        }
-    }
+}
