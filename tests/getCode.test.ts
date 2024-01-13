a8e89f4c0482fdca8c0494575ee55449ca5d9cd1
Vorobev Dmiyrii <serparns@gmail.com>
2024-01-13 12:08:44 +0300

    fix

--- tests/getCode.test.ts
+++ tests/getCode.test.ts
@@ -3,45 +3,63 @@ import {getRandomEmail, getRandomPhoneNumber} from "../utils/random";
 import UserRequests from "../requests/user.requests";
 import {getBaseParameters} from "../entities/baseParameters";
 import ClubsRequests from "../requests/clubs.requests";
+import VerifyRequests from "../requests/verify.request";
 
-const mocUserData = {
-    session_id: "123",
-    request_id: "123",
-    request_source: "crm",
-    data: {
-        email: '',
-        name: "Имя",
-        last_name: "last_name",
-        middle_name: "string",
-        sex: "male",
-        phone: '',
-        birthday: "1999-11-11",
-        password: "qwerty123",
-        lang: "ru",
-        club_access: true,
-        admin_panel_access: true,
-        group_training_registration_access: true,
-        sport_experience: "Нет опыта",
-    }
-}
 
-test.describe("Api-тест на создание юзера с клубом и получения данных о нем", async () => {
+test.describe("Api-тесты на получение кода верификации", async () => {
+    test.only("[positive] отправка кода верификации клиенту", async ({request}) => {
+        const clubId = await test.step("Получить id клуба", async  () => {
+            const parameters = { ...await getBaseParameters() };
+            const getClubResponse = await new ClubsRequests(request).getClubById(200, parameters);
+            const getClubData =  await getClubResponse.json();
+            return  getClubData?.data[0]?.id;
+        });
 
-    test.beforeEach(() => {
-        mocUserData.data.phone = getRandomPhoneNumber()
-        mocUserData.data.email = getRandomEmail()
-    });
+        const {userId, userPhone} = await test.step("Получить id клиента", async  () => {
+            const requestBody = {
+                session_id: "123",
+                request_id: "321",
+                request_source: "crm",
+                data: {
+                    email: getRandomEmail(),
+                    name: "Имя",
+                    last_name: "last_name",
+                    middle_name: "string",
+                    sex: "male",
+                    phone: getRandomPhoneNumber(),
+                    birthday: "1999-11-11",
+                    password: "qwerty123",
+                    lang: "ru",
+                    club_access: true,
+                    admin_panel_access: true,
+                    group_training_registration_access: true,
+                    sport_experience: "Нет опыта",
+                    home_club_id: clubId
+                }
+            }
+            const response = (await (await new UserRequests(request).postCreateUser(200, requestBody)).json()).data
+            return {
+                userId: response.id,
+                userPhone: response.phone
+            }
+        });
 
-    test("[positive] V2__получить юзера с подстановкой idклуба из запроса", async ({request}) => {
-        const parameters = {...await getBaseParameters()};
-        const getClubResponse = await new ClubsRequests(request).getClubById(200, parameters);
-        const getClubData = getClubResponse.json();
-        const clubId = getClubData?.data[0]?.id;
-        const requestBody = {...mocUserData, data: {...mocUserData, home_club_id: clubId}}
-        const postUserResponse = await new UserRequests(request).postCreateUser(200, requestBody);
-        const {data: postUserData = {}} = await postUserResponse.json();
-        const userId = postUserData?.id;
-        const getUserResponse = await new UserRequests(request).getUser(200, parameters, userId);
-        const {data: getUserData = {}} = await getUserResponse.json();
+            const response = await test.step("Отправить код верификации клиенту", async () => {
+                const requestBody = {
+                    session_id: "123",
+                    request_id: "321",
+                    request_source: "crm",
+                    data: {
+                        message_type: "sms",
+                        contact: userPhone,
+                        template: "mail_signing_an_agreement",
+                        user_id: userId
+                    }
+                }
 
+                const response = await new VerifyRequests(request).postGetCode(200, requestBody);
+                return response.json()
+            });
+                expect(response.status).toEqual('OK');
+        });
     });
--- tests/getUser.test.ts
+++ tests/getUser.test.ts
@@ -4,47 +4,47 @@ import UserRequests from "../requests/user.requests";
 import {getBaseParameters} from "../entities/baseParameters";
 import ClubsRequests from "../requests/clubs.requests";
 
-const mocUserData = {
-    session_id: "123",
-    request_id: "123",
-    request_source: "crm",
-    data: {
-        email: '',
-        name: "Имя",
-        last_name: "last_name",
-        middle_name: "string",
-        sex: "male",
-        phone: '',
-        birthday: "1999-11-11",
-        password: "qwerty123",
-        lang: "ru",
-        club_access: true,
-        admin_panel_access: true,
-        group_training_registration_access: true,
-        sport_experience: "Нет опыта",
-    }
-}
+// const mocUserData = {
+//     session_id: "123",
+//     request_id: "123",
+//     request_source: "crm",
+//     data: {
+//         email: '',
+//         name: "Имя",
+//         last_name: "last_name",
+//         middle_name: "string",
+//         sex: "male",
+//         phone: '',
+//         birthday: "1999-11-11",
+//         password: "qwerty123",
+//         lang: "ru",
+//         club_access: true,
+//         admin_panel_access: true,
+//         group_training_registration_access: true,
+//         sport_experience: "Нет опыта",
+//     }
+// }
 
 test.describe("Api-тест на создание юзера с клубом и получения данных о нем", async () => {
 
-    test.beforeEach(() => {
-        mocUserData.data.phone = getRandomPhoneNumber()
-        mocUserData.data.email = getRandomEmail()
-    });
-
-    test("[positive] V2__получить юзера с подстановкой idклуба из запроса", async ({request}) => {
-        const parameters = {...await getBaseParameters()};
-        const getClubResponse = await new ClubsRequests(request).getClubById(200, parameters);
-        const getClubData = getClubResponse.json();
-        const clubId = getClubData?.data[0]?.id;
-        const requestBody = {...mocUserData, data: {...mocUserData, home_club_id: clubId}}
-        const postUserResponse = await new UserRequests(request).postCreateUser(200, requestBody);
-        const {data: postUserData = {}} = await postUserResponse.json();
-        const userId = postUserData?.id;
-        const getUserResponse = await new UserRequests(request).getUser(200, parameters, userId);
-        const {data: getUserData = {}} = await getUserResponse.json();
-
-    });
+    // test.beforeEach(() => {
+    //     mocUserData.data.phone = getRandomPhoneNumber()
+    //     mocUserData.data.email = getRandomEmail()
+    // });
+    //
+    // test("[positive] V2__получить юзера с подстановкой idклуба из запроса", async ({request}) => {
+    //     const parameters = {...await getBaseParameters()};
+    //     const getClubResponse = await new ClubsRequests(request).getClubById(200, parameters);
+    //     const getClubData = await getClubResponse.json();
+    //     const clubId = getClubData?.data[0]?.id;
+    //     const requestBody = {...mocUserData, data: {...mocUserData, home_club_id: clubId}}
+    //     const postUserResponse = await new UserRequests(request).postCreateUser(200, requestBody);
+    //     const {data: postUserData = {}} = await postUserResponse.json();
+    //     const userId = postUserData?.id;
+    //     const getUserResponse = await new UserRequests(request).getUser(200, parameters, userId);
+    //     const {data: getUserData = {}} = await getUserResponse.json();
+    //
+    // });
 
     test("[positive] получить юзера с подстановкой idклуба из запроса", async ({request}) => {
         const getClubs = await new ClubsRequests(request).getClubById(200, await getBaseParameters());
