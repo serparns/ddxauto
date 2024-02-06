fe953667f3ddacf8276be6f87c81d813d8b9bfa4
Vorobev Dmiyrii <serparns@gmail.com>
2024-02-06 22:41:44 +0300

    new ui test

--- tests/ui/klientInClub.test.ts
+++ tests/ui/klientInClub.test.ts
@@ -2,15 +2,13 @@ import {test} from "@playwright/test";
 import api from "@api"
 import authCRMTestData from "@data/authCRM.json"
 
-test.describe("Тесты на авторизацию в CRM", async () => {
+test.describe("Тест на переход в клиенты в клубе", async () => {
     test("Успешная авторизация в CRM", async ({page}) =>{
-       await test.step("Перейти на страницу входа", async ()  =>{
+        await test.step("Перейти на страницу входа", async ()  =>{
             await page.goto(`${api.urls.base_url_CRM}`)
-
 
         });
         await test.step("Заполнить форму авторизации и нажать зайти", async () =>{
-            await page.getByPlaceholder("Логин").waitFor({state: "visible", timeout: 3000});
             await page.getByPlaceholder("Логин").fill(authCRMTestData.login);
             await page.getByPlaceholder("Пароль").fill(authCRMTestData.password);
             await page.getByRole('button', {name: 'Войти'}).click()
@@ -18,6 +16,11 @@ test.describe("Тесты на авторизацию в CRM", async () => {
 
         await test.step("Проверить что пользователь находится в CRM и видит поле поиска", async () =>{
             await page.locator("//input[@data-testid='phone-input']").waitFor({state: "visible", timeout: 3000})
+        });
+
+        await test.step("Проверить что пользователь находится на странице клиенты в клубе и видит фильтры", async () =>{
+            await page.locator("//div/a[@href='/clients-in-club']").click()
+            await page.getByRole('button', {name: 'Фильтры'}).waitFor({state: "visible", timeout: 3000})
         });
     });
 });
