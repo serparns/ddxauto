c6f12a3b83f02ad414dcefde9e22171761abdd64
Vorobev Dmiyrii <serparns@gmail.com>
2024-03-24 11:10:34 +0300

    /

--- tests/ui/pageNavigation/menuCrm.test.ts
+++ tests/ui/pageNavigation/menuCrm.test.ts
@@ -6,7 +6,7 @@ test.describe("Тест на навигацию по боковому меню",
     test("Успешный переход по всем ссылкам из меню", async ({ page }) => {
         await test.step("Перейти на страницу входа", async () => {
             await page.goto(`${api.urls.base_url_CRM}`);
-
+            
         });
         await test.step("Заполнить форму авторизации и нажать зайти", async () => {
             await page.getByPlaceholder("Логин").fill(authCRMTestData.login);
