8197da1445a0b8ad07cc3ab6a63a95a4021548c5
Vorobev Dmiyrii <serparns@gmail.com>
2024-06-15 18:56:01 +0300

    fix global-setup

--- global-setup.ts
+++ global-setup.ts
@@ -8,9 +8,8 @@ async function globalSetup() {
     const browser = await chromium.launch();
     const page = await browser.newPage();
     const request = page.request;
-    const groupTrainingData = (await (await new GroupTrainingRequests(request).getGroupTraining(Statuses.OK, await getBaseParameters())).json()).data[0]
 
     process.env['CLUB_ID'] = (await (await new ClubsRequests(request).getClubs(Statuses.OK, await getBaseParameters())).json()).data[0].id
-    process.env['GROUP_TRAINING_DATA'] = JSON.stringify(groupTrainingData)
+    process.env['GROUP_TRAINING_DATA'] = JSON.stringify((await (await new GroupTrainingRequests(request).getGroupTraining(Statuses.OK, await getBaseParameters())).json()).data[0])
 }
 export default globalSetup;
